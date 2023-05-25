5f9594bc5ed86bc83a40f5af31849f85c788c61f
LucidOtter <taronsilver@hotmail.ch>
2023-05-25 12:26:29 +0200

    Moved to jupyter lab Added comments and markdown explanations

--- 02_end_to_end_ML_learning_project.py
+++ 02_end_to_end_ML_learning_project.py
@@ -5,14 +5,17 @@ import tarfile
 import urllib.request
 
 def load_housing_data():
-    tarball_path=Path("datasets/housing.tgz")
+    tarball_path=Path("./datasets/housing.tgz")
     if not tarball_path.is_file():
-        Path("datasets").mkdir(parents=True, exist_ok=True)
-        url="https:github.com/ageron/data/raw/main/housing.tgz"
+        Path("./datasets").mkdir(parents=True, exist_ok=True)
+        url="https://github.com/ageron/data/raw/main/housing.tgz"
 
         urllib.request.urlretrieve(url, tarball_path)
         with tarfile.open(tarball_path) as housing_tarball:
             housing_tarball.extractall(path="datasets")
-    return pd.read_csv(Path("datasets/housing/housing.csv"))
+    print(Path("./datasets/housing/housing.csv"))
+    return pd.read_csv(Path("./datasets/housing/housing.csv"))
 
 housing = load_housing_data()
+
+print('a')
